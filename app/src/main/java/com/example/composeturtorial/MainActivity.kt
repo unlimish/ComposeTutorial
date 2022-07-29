dd312bffe24ced702e033620942de89e93177a3c
unlimish <me@unlimi.sh>
2022-07-30 01:21:59 +0900

    Sharing status between functions

--- app/src/main/java/com/example/composeturtorial/MainActivity.kt
+++ app/src/main/java/com/example/composeturtorial/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.composeturtorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.OutlinedButton
 import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.material.Button
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
@@ -33,10 +31,39 @@ class MainActivity : ComponentActivity() {
     }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Kotlin")) {
+fun MyApp() {
+    var shouldShowWelcome by  remember { mutableStateOf(true) }
+
+    if (shouldShowWelcome) {
+        WelcomeScreen(onContinueClicked = {shouldShowWelcome = false})
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+private fun Greetings(names: List<String> = listOf("World", "Kotlin")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)}
+    }
+}
+
+@Composable
+fun WelcomeScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+           Text("Welcome to the Basics Codelab!")
+           Button(
+               modifier = Modifier.padding(vertical = 24.dp),
+               onClick = onContinueClicked
+           ) {
+               Text("Continue")
+           } 
+        }
     }
 }
 
@@ -50,7 +77,7 @@ fun Greeting(name: String){
         Row(modifier= Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding( bottom = extraPadding)) {
+                .padding(bottom = extraPadding)) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
@@ -63,7 +90,7 @@ fun Greeting(name: String){
     }
 }
 
-@Preview(widthDp = 320)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
