38f6dc0c55b705715e394079c4cbc58f95ad3486
unlimish <me@unlimi.sh>
2022-07-29 16:09:07 +0900

    ♻ Refactor

--- app/src/main/java/com/example/composeturtorial/MainActivity.kt
+++ app/src/main/java/com/example/composeturtorial/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.example.composeturtorial
 
 import android.os.Bundle
-import android.support.v4.os.IResultReceiver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeturtorial.ui.theme.ComposeTurtorialTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -24,11 +17,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
+    }
+
+@Composable
+private fun MyApp() {
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
     }
 }
 
@@ -42,8 +40,8 @@ private fun Greeting(name: String){
 
 @Preview
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     MaterialTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
