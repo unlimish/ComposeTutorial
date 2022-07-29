5087edd568ee18faf9bf86fb69cfdf731d0631ac
unlimish <me@unlimi.sh>
2022-07-30 02:26:49 +0900

    🎨 Update: theming

--- app/src/main/java/com/example/composeturtorial/MainActivity.kt
+++ app/src/main/java/com/example/composeturtorial/MainActivity.kt
@@ -3,27 +3,34 @@ package com.example.composeturtorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.OutlinedButton
 import androidx.compose.ui.modifier.modifierLocalOf
-import androidx.compose.material.Button
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import kotlin.math.exp
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import com.example.composeturtorial.ui.theme.ComposeTurtorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            ComposeTurtorialTheme {
                     MyApp()
                 }
             }
@@ -32,7 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowWelcome by  remember { mutableStateOf(true) }
+    var shouldShowWelcome by  rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowWelcome) {
         WelcomeScreen(onContinueClicked = {shouldShowWelcome = false})
@@ -42,10 +49,9 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Kotlin")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)}
+private fun Greetings(names: List<String> = List(1000) {"$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) {name -> Greeting(name = name)}
     }
 }
 
@@ -69,22 +75,34 @@ fun WelcomeScreen(onContinueClicked: () -> Unit) {
 
 @Composable
 fun Greeting(name: String){
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(color = MaterialTheme.colors.primary,
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier= Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            IconButton(
+                onClick = { expanded = !expanded }) {
+                Icon(imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                    stringResource(R.string.show_more)
+                })
             }
         }
     }
@@ -93,7 +111,7 @@ fun Greeting(name: String){
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
+    ComposeTurtorialTheme() {
         MyApp()
     }
 }
